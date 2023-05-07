7bfc9528a47cfca0e06d1d3d6697b24b4b410b83
Douglas <douglasrosso10@gmail.com>
2023-05-07 02:46:42 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
+
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 100) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "../assets/game-over.png";
+    mario.style.width = "60px";
+    mario.style.marginLeft = "50px";
+
+    clearInterval(loop);
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  if (event.code === "ArrowUp") {
+    jump();
+  }
+});
